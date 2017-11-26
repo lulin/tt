0c17d9183413a3bc622bbb94fe323cfcaf5980a5
Lulin Wang <lulin_wa@163.com>
2017-11-26 17:53:23 +0800

    rename doc to docs

--- src/racket/lib/config.rkt
+++ src/racket/lib/config.rkt
@@ -1,16 +1,25 @@
 #lang racket/base
 
-(require racket/contract)
+(require racket/contract
+         racket/match
+         json)
 
 (provide (contract-out
-          [get-config (-> (hash/c symbol? any/c))]))
+          [get-config (-> (hash/c symbol? any/c))])
+         default-init-config-paths
+         init-config-path)
 
 (define default-init-config-paths
   (make-parameter (list "./tt.conf"
                         "~/.tt/tt.conf"
                         "/etc/tt/tt.conf")))
 
-(define init-config-path)
+(define init-config-path
+  (make-parameter (match (default-init-config-paths)
+                    [(? string? str) str]
+                    [(list head _ ...) head]
+                    [_ 'null])))
 
 (define (get-config)
   #hasheq((a . 1)))
+
