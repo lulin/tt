a26aa2d13cab6b43370857ffd823c8959fe35d66
Lulin Wang <lulin_wa@163.com>
2017-11-27 14:15:16 +0800

    config.rkt

--- src/racket/lib/config.rkt
+++ src/racket/lib/config.rkt
@@ -4,22 +4,42 @@
          racket/match
          json)
 
-(provide (contract-out
-          [get-config (-> (hash/c symbol? any/c))])
-         default-init-config-paths
-         init-config-path)
+(provide parse-config-file
+         ;; parameter specifies the initial config path
+         init-config-path
+         search-config-file)
 
+(define init-config-path (make-parameter #f))
+
+;; Search the configuration file.
+(define (search-config-file [path (init-config-path)])
+  (cond [path (if (path-exists? path)
+                  path
+                  #f)]
+        [else
+         (for/fold ([path #f])
+                   ([item default-init-config-paths]
+                    #:unless path)
+           (if (path-exists? item)
+               item
+               #f))]))
+
+;; expand a path before checking it
+(define (path-exists? path)
+  (if (not path)
+      #f
+      (file-exists? (expand-user-path path))))
+
+;; Parse config file
+(define (parse-config-file [conf (search-config-file)])
+  (if (not conf)
+      #f
+      (read-json (open-input-file (expand-user-path conf)))))
+
+;; default initial configuration path
 (define default-init-config-paths
-  (make-parameter (list "./tt.conf"
-                        "~/.tt/tt.conf"
-                        "/etc/tt/tt.conf")))
+  (list "./tt.conf"
+        "~/.tt/tt.conf"
+        "/etc/tt/tt.conf"))
 
-(define init-config-path
-  (make-parameter (match (default-init-config-paths)
-                    [(? string? str) str]
-                    [(list head _ ...) head]
-                    [_ 'null])))
-
-(define (get-config)
-  #hasheq((a . 1)))
 
--- src/racket/lib/data.rkt
+++ src/racket/lib/data.rkt
@@ -2,9 +2,5 @@
 
 (require json)
 
-(call-with-output-string
- (lambda (out)
-   (write "abc" out)
-   (get-output-string out)))
-
+;; handler
 
